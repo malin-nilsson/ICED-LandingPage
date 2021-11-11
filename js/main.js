fd3dab3cd8a2c024bc62b1adae5ce01a4e56b0ba
”Malin <malinnilsson87@gmail.com>
2021-11-11 19:17:17 +0100

    Added window onload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
+window.onload = function() {
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+}
