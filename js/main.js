f5b0e87bea92ec1d8d3c440b2443fc2e080f0fc8
Malin Nilsson <malinnilsson87@gmail.com>
2021-11-24 21:33:58 +0100

    Added menu pop up modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 window.onload = function () {
-  const toggleButton = document.getElementsByClassName('toggle-button')[0]
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-  toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-  })
+// Get the modal
+let modal = document.getElementById("menuModal");
+let modal2 = document.getElementById("locationModal")
+// Get the button that opens the modal
+let btn = document.getElementById("modalButton");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
+
+}
+
