019bf7bfc5a04e10b4a9ced4e8f6ddb2170e9dc4
Malin Nilsson <malinnilsson87@gmail.com>
2021-11-30 21:58:47 +0100

    Added parcel

---
+++ src/ts/main.ts
@@ -0,0 +1,46 @@
+window.onload = function () {
+    menuModal() 
+    locationsModal()
+  }
+  
+  function menuModal() {
+    let menuModal = document.getElementById("menu-modal");
+    let btnMenu = document.getElementById("btn-menu");
+    let span = document.querySelectorAll(".close")[0];
+  
+    btnMenu.addEventListener("click", () => {
+      menuModal.style.display = "block";
+    }) 
+  
+    span.addEventListener("click", () => {
+      menuModal.style.display = "none";
+    }) 
+  
+    window.onclick = function(event) {
+      if (event.target == menuModal) {
+        menuModal.style.display = "none";
+      }
+    } 
+  }
+  
+  function locationsModal() {
+    let locationsModal = document.getElementById("locations-modal");
+    let btnLocations = document.getElementById("btn-locations");
+    let span = document.getElementsByClassName("close")[1];
+  
+    btnLocations.addEventListener("click", () => {
+      locationsModal.style.display = "block";
+    }) 
+  
+    span.addEventListener("click", () => {
+      locationsModal.style.display = "none";
+    }) 
+  
+    window.onclick = function(event) {
+      if (event.target == locationsModal) {
+        locationsModal.style.display = "none";
+      }
+    } 
+  }
+  
+  
