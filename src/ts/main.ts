9ee4e3c9ff8886596caba678c33bf01cf619aaa6
Malin Nilsson <malinnilsson87@gmail.com>
2021-12-17 19:21:21 +0100

    Added animation to modals

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,46 +1,44 @@
 window.onload = function () {
-    menuModal() 
-    locationsModal()
-  }
-  
-  function menuModal() {
-    let menuModal = document.getElementById("menu-modal");
-    let btnMenu = document.getElementById("btn-menu");
-    let span = document.querySelectorAll(".close")[0];
-  
-    btnMenu.addEventListener("click", () => {
-      menuModal.style.display = "block";
-    }) 
-  
-    span.addEventListener("click", () => {
+  menuModal()
+  locationsModal()
+}
+
+function menuModal() {
+  let menuModal: HTMLDivElement = document.getElementById("menu-modal") as HTMLDivElement;
+  let menuButton: HTMLButtonElement = document.getElementById("btn-menu") as HTMLButtonElement;
+  let closeMenuModal: HTMLSpanElement = document.querySelector(".close-menu-modal") as HTMLSpanElement;
+
+  menuButton.addEventListener("click", () => {
+    menuModal.style.display = "block";
+  })
+
+  closeMenuModal.addEventListener("click", () => {
+    menuModal.style.display = "none";
+  })
+
+  window.addEventListener('click', function (event) {
+    if (event.target == menuModal) {
       menuModal.style.display = "none";
-    }) 
-  
-    window.onclick = function(event) {
-      if (event.target == menuModal) {
-        menuModal.style.display = "none";
-      }
-    } 
-  }
-  
-  function locationsModal() {
-    let locationsModal = document.getElementById("locations-modal");
-    let btnLocations = document.getElementById("btn-locations");
-    let span = document.getElementsByClassName("close")[1];
-  
-    btnLocations.addEventListener("click", () => {
-      locationsModal.style.display = "block";
-    }) 
-  
-    span.addEventListener("click", () => {
+    }
+  });
+}
+
+function locationsModal() {
+  let locationsModal: HTMLDivElement = document.getElementById("locations-modal") as HTMLDivElement;
+  let locationsButton: HTMLButtonElement = document.getElementById("btn-locations") as HTMLButtonElement;
+  let closeLocationsModal: HTMLSpanElement = document.querySelector(".close-locations-modal");
+
+  locationsButton.addEventListener("click", () => {
+    locationsModal.style.display = "block";
+  })
+
+  closeLocationsModal.addEventListener("click", () => {
+    locationsModal.style.display = "none";
+  })
+
+  window.addEventListener('click', function (event) {
+    if (event.target == locationsModal) {
       locationsModal.style.display = "none";
-    }) 
-  
-    window.onclick = function(event) {
-      if (event.target == locationsModal) {
-        locationsModal.style.display = "none";
-      }
-    } 
-  }
-  
-  
+    }
+  });
+}
